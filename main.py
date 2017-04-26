d3d38e5cafdbd87563ecf3aa24197143c5b7c58d
ColtonKinstley <colton.kinstley@gmail.com>
2017-04-26 15:25:11 -0500

    Updated Slides a little

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import pandas as pd
 import sklearn as sk
 import matplotlib.pyplot as plt
+import numpy as np
+
+
+def nSampleFromY(beta, n, sd):
+    p = len(beta)
+    designMatrix = np.array([])
+    matrix = 
+    for i in range(p):
+        designMatrix[i] = np.random.normal(0,1)*beta[i] 
+    y = sum(designMatrix) + np.random.normal(0,sd)
+        
+
 
 #do some stuff
 
